43460c0c4145163fbb7738cb2e14b042ad6d4b8e
erjan123 <erjan123@yahoo.com>
2018-11-28 14:14:33 +0200

    Linq11() projection Operator example

--- LINQ - Projection Operators/C#/ProjectionOperators/LinqSamples/DataSetLinq11.cs
+++ LINQ - Projection Operators/C#/ProjectionOperators/LinqSamples/DataSetLinq11.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ComponentModel;
 using System.Data;
+using System.Linq;
 
 namespace ProjectionOperators
 {
     public partial class LinqSamplesProjectionOperators
     {
-        [Category("Projection Operators")]
-        [Description("This sample uses select to produce a sequence containing some properties " +
-             "of Products, including UnitPrice which is renamed to Price " +
-             "in the resulting type.")]
+        //Projection Operators
+        //This sample uses select to produce a sequence containing some properties
+        //of Products, including UnitPrice which is renamed to Price
+        //in the resulting type.
         public void DataSetLinq11()
         {
             var products = testDS.Tables["Products"].AsEnumerable();
@@ -32,6 +29,30 @@ namespace ProjectionOperators
                 {
                     Console.WriteLine("{0} is in the category {1} and costs {2} per unit.", productInfo.ProductName, productInfo.Category, productInfo.Price);
                 } 
+
+            #endregion
+        }
+
+        // Query syntax
+        public void DataSetLinq11A()
+        {
+            var products = testDS.Tables["Products"].AsEnumerable();
+
+            #region  Make Sure to try yourself before looking at the code
+
+                var productInfos = from n in products
+                                   select( new {
+                                                ProductName = n.Field<string>("ProductName"),
+                                                Category = n.Field<string>("Category"),
+                                                Price = n.Field<decimal>("UnitPrice")
+                                            });
+                Console.WriteLine();
+                Console.WriteLine("******************************");
+                Console.WriteLine("With Query Syntax - Product Info:");
+                foreach (var productInfo in productInfos)
+                {
+                    Console.WriteLine("{0} is in the category {1} and costs {2} per unit.", productInfo.ProductName, productInfo.Category, productInfo.Price);
+                }
 
             #endregion
         }
